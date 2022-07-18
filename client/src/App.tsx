40a24308b5fcaa4e7c65ff28f082c4ed263744b3
Vincy-fivestones <107828540+Vincy-fivestones@users.noreply.github.com>
2022-07-18 17:25:48 +0800

    set axios POST request

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 function App() {
+  const [link, setLink] = useState('');
+
+  const handleChange = (event: React.FormEvent): void => {
+    setLink((event.target as HTMLInputElement).value);
+  };
+  const handleSubmit = async (event: React.FormEvent): Promise<void> => {
+    event.preventDefault();
+    const url = '/youtube';
+    axios
+      .post(url, {
+        youtubelink: link,
+      })
+      .then((re) => {
+        setLink('');
+        (event.target as HTMLFormElement).reset();
+      })
+      .catch((e) => {});
+  };
   return (
     <div className="App">
-      <div>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+      <Form className="text-start m-4" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Youtube Link</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter link"
+            value={link}
+            onChange={handleChange}
+          />
+          {/* <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text> */}
+        </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </div>
+        {/* <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group> */}
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
